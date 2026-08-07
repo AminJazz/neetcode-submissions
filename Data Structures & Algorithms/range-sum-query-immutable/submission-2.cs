5f989fefb7c79ac1e0cc81e7f5ba97feeae44631
AminJazz <amin4cs@gmail.com>
2026-08-07 14:59:46 +0530

    Add: range-sum-query-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-2.cs
@@ -0,0 +1,23 @@
+public class NumArray {
+    private int[] arr = new int[1];
+    public NumArray(int[] nums) {
+        arr = nums;
+    }
+    
+    public int SumRange(int left, int right) {
+        int total = 0;
+
+        while (left <= right) {
+            total += arr[left];
+            left++;
+        }
+
+        return total;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
