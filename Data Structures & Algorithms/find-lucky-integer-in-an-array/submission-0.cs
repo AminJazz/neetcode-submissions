3997bb9fd385518f1aad74e55a9b5a8df33b6d79
AminJazz <amin4cs@gmail.com>
2026-08-11 08:59:32 +0530

    Add: find-lucky-integer-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int FindLucky(int[] arr) {
+        int[] count = new int[500];
+
+        for (int i = 0; i < arr.Length; i++) {
+            count[arr[i]]++;
+        }
+
+        int max = - 1;
+        for (int i = 1; i < count.Length; i++) {
+            if (count[i] == i) max = i;
+        }
+
+        return max;
+    }
+}
