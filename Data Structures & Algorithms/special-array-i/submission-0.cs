fe25be639a1168d32bfd787a146eef03269ee16f
AminJazz <amin4cs@gmail.com>
2026-08-11 09:06:52 +0530

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.Length; i++) {
+            if ((nums[i] % 2 == 0 && nums[i - 1] % 2 == 0) ||
+                (nums[i] % 2 != 0 && nums[i - 1] % 2 != 0))
+                return false;
+        }
+
+        return true;
+    }
+}
