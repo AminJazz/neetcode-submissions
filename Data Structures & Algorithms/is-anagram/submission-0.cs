18bbb00fb15f05ccffb172d9e78103180e465108
AminJazz <amin4cs@gmail.com>
2026-08-22 10:59:25 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+
+        int[] arr = new int[26];
+        for (int i = 0; i < t.Length; i++) {
+            arr[t[i] - 'a']++;
+            arr[s[i] - 'a']--;
+        }
+
+        for (int i = 0; i < arr.Length; i++) {
+            if (arr[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
