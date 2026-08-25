4f11a783ed4304e2999a59e4614857db3a5f0a66
AminJazz <amin4cs@gmail.com>
2026-08-25 22:34:17 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string s = strs[0];
+        for (int i = 1; i < strs.Length; i++) {
+            while (!strs[i].StartsWith(s)) {
+                s = s.Substring(0, s.Length - 1);
+                if (s == "") return "";
+            }
+        }
+
+        return s;
+    }
+}
