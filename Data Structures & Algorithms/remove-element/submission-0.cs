0eefd456cacecfef8fc77a01adf63625fd973451
AminJazz <amin4cs@gmail.com>
2026-08-26 22:28:07 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int p = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != val) {
+                (nums[p], nums[i]) = (nums[i], nums[p]);
+                p++;
+            }
+        }
+
+        return p;
+    }
+}
