6287dacde6f267d38e62828b11e347fdf2527fd1
AminJazz <amin4cs@gmail.com>
2026-09-03 09:20:10 +0530

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        if (flowerbed.Length == 1 && flowerbed[0] == 0) return true;
+
+        for (int i = 1; i < flowerbed.Length - 1; i++) {
+            if (i == 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0) {
+                flowerbed[i - 1] = 1;
+                n--;
+            }
+            else if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 &&
+                flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            else if (i == flowerbed.Length - 2 && flowerbed[i] == 0 &&
+                flowerbed[i + 1] == 0) {
+                    flowerbed[i + 1] = 0;
+                    n--;
+            }
+        }
+
+        return n <= 0;
+    }
+}
