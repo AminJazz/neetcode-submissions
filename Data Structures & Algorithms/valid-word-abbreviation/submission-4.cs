c3d09136c9d5335d46d85ee8132c62294aa0b857
AminJazz <amin4cs@gmail.com>
2026-09-05 23:32:51 +0530

    Add: valid-word-abbreviation - submission-4

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-4.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool ValidWordAbbreviation(string word, string abbr) {
+        int l = 0, r = 0;
+
+        while (l < word.Length) {
+            if (r > abbr.Length - 1) return false;
+            else if (r < abbr.Length && word[l] == abbr[r]) {
+                l++;
+                r++;
+            }
+            else if (char.IsDigit(abbr[r])) {
+                int i = abbr[r++] - '0';
+                if (i == 0) return false;
+                
+                while (r < abbr.Length && char.IsDigit(abbr[r])) {
+                    i = i * 10 + (abbr[r++] - '0');
+                }
+
+                if (i + l > word.Length) return false;
+                l += i;
+            }
+            else return false;
+        }
+
+        return r == abbr.Length ? true : false;
+    }
+}
