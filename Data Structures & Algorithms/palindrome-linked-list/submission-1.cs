86892fe211c998790000097bfe277b07cbc8a4d8
AminJazz <amin4cs@gmail.com>
2026-09-18 11:20:45 +0530

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode reverse = ReverseLinkedList(slow);
+        while (reverse != null) {
+            if (head.val != reverse.val) return false;
+            head = head.next;
+            reverse = reverse.next;
+        }
+        
+        return true;
+    }
+
+    public ListNode ReverseLinkedList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
